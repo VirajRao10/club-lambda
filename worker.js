2d65fa5aaa859bd379e05d337f2ee08cead288af
Viraj <raoco@virajrao.com>
2026-06-27 18:00:06 +0200

    Clone casino experience

--- worker.js
+++ worker.js
@@ -1,4 +1,6 @@
 import html from "./index.html";
+import css from "./app-css.txt";
+import app from "./app-client.txt";
 
 const PATH_PREFIXES = ["/gamble", "/clublambda"];
 
@@ -11,6 +13,24 @@ export default {
 
     if (!prefix) {
       return new Response("Not found", { status: 404 });
+    }
+
+    if (url.pathname === `${prefix}/app.css`) {
+      return new Response(css, {
+        headers: {
+          "content-type": "text/css; charset=utf-8",
+          "cache-control": "public, max-age=120"
+        }
+      });
+    }
+
+    if (url.pathname === `${prefix}/app.js`) {
+      return new Response(app.replaceAll("__CLUB_LAMBDA_BASE__", prefix), {
+        headers: {
+          "content-type": "application/javascript; charset=utf-8",
+          "cache-control": "public, max-age=120"
+        }
+      });
     }
 
     const headers = new Headers({
